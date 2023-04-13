75b245978ed23a68fb13aa344a2f9d033e48e82e
Jeanine19 <108534306+Jeanine19@users.noreply.github.com>
2023-04-13 12:21:52 -0300

    Update PostagemRepository.java

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,7 +3,7 @@ package com.generation.blogpessoal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
-
+@repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 }
